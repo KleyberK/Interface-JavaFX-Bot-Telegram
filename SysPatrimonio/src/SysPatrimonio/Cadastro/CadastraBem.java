584fa9f76dbb8ac728c367ebd8deaef3e9e5ba83
Kleyber José Gomes Marçal <51139128+KleyberK@users.noreply.github.com>
2019-11-26 19:44:34 -0300

    Complemento de Interface e fechamento de tela

--- SysPatrimonio/src/SysPatrimonio/Cadastro/CadastraBem.java
+++ SysPatrimonio/src/SysPatrimonio/Cadastro/CadastraBem.java
@@ -4,36 +4,54 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class CadastraBem {
+    @FXML
     public javafx.scene.control.TextField fieldNome;
+    @FXML
     public javafx.scene.control.TextField fieldDescricao;
+    @FXML
     public javafx.scene.control.TextField fieldCategoria;
+    @FXML
     public javafx.scene.control.TextField fieldLocal;
+    @FXML
+    private javafx.scene.control.Button OKButton;
+    @FXML
+    private javafx.scene.control.Button CancelButton;
+
+    Stage primaryStage = new Stage();
 
     public void start() throws Exception {
-        Stage primaryStage = new Stage();
         Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("cadastraBem.fxml"));
         primaryStage.setTitle("Cadastrar Bem");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
 
-        JButton OKButton = new JButton();
+    @FXML
+    protected void OKclique(){
+        Stage stage = (Stage) OKButton.getScene().getWindow();
+        fieldNome.getText();
+        fieldDescricao.getText();
+        fieldCategoria.getText();
+        fieldLocal.getText();
+        System.out.println(fieldNome.getText());
+        System.out.println(fieldDescricao.getText());
+        System.out.println(fieldCategoria.getText());
+        System.out.println(fieldLocal.getText());
+        stage.close();
+    }
 
-        if(OKButton.isSelected()){
-            fieldNome.getText();
-            fieldDescricao.getText();
-            fieldCategoria.getText();
-            fieldLocal.getText();
-            System.out.println(fieldNome);
-            System.out.println(fieldDescricao);
-            System.out.println(fieldCategoria);
-            System.out.println(fieldLocal);
-        }
+    @FXML
+    protected void Cancelclique(){
+        Stage stages = (Stage) CancelButton.getScene().getWindow();
+        stages.close();
     }
 
 }
